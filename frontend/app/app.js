94543671f1ce03f31c8eef905f0995da84b409a8
pajowu <github@ca.pajowu.de>
2016-06-15 19:59:16 +0300

    add inital version of DataFactory

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,7 +2,28 @@ var hackvoteApp = angular.module('hackvote', ['ngRoute', 'ngWebSocket', 'eventLi
 
 //DataFactory stores all data and cares about refreshing it
 hackvoteApp.factory('DataFactory', ['$websocket', function($websocket) {
-    var methods = {};
+    "use strict";
+
+    var dataStream = $websocket('ws://localhost:9000');
+
+    var events, projects = [];
+
+    dataStream.onMessage(function(message) {
+        console.log(message);
+    });
+
+    function sendData(data) {
+        dataStream.send(JSON.stringify(data));
+    }
+    var methods = {
+        events: events,
+        get_events: function() {
+            sendData({ action: 'get_projects' });
+        },
+        get_event: function() {
+            sendData({ action: 'get_projects' });
+        }
+    };
 
     return methods;
 }]);
--- frontend/app/event-list/event-list.js
+++ frontend/app/event-list/event-list.js
@@ -1,10 +1,9 @@
-angular.module('eventList', ['ngRoute', 'DataFactory'])
-.component('eventList', ["DataFactory"], function (DataFactory) {
-	return {
+angular.module('eventList', [])
+.component('eventList', {
     templateUrl: 'app/event-list/event-list.template.html',
     controller: function EventListController(DataFactory) {
         var self = this;
         DataFactory.get_events();
         self.events = DataFactory.events;
     }
-}});
+});
